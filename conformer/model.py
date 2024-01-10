d5ca46fdd544ee271a4ca2a9757e66be5c2ab283
oelin <42823429+oelin@users.noreply.github.com>
2024-01-10 14:22:13 +0000

    Update model.py

--- conformer/model.py
+++ conformer/model.py
@@ -5,14 +5,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from einops import reduce, rearrange
+from einops import reduce, repeat, rearrange
 from einops.layers.torch import Rearrange
-
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
-from einops import rearrange, repeat
 
 
 class RoPE(nn.Module):
